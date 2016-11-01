6ecf7f7326b29f3e45a2110bc012bf14149a7ff0
Brandon John-Freso <brandon@okcupid.com>
2016-11-01 02:19:16 -0400

    flesh out the SC API interface

--- app/src/main/java/com/brandonjf/cloudtrack/data/SoundCloudApiInterface.java
+++ app/src/main/java/com/brandonjf/cloudtrack/data/SoundCloudApiInterface.java
@@ -1,9 +1,17 @@
 package com.brandonjf.cloudtrack.data;
 
+import com.brandonjf.cloudtrack.model.UserResponse;
+
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+import rx.Observable;
+
 /**
  * Created by brandon on 10/31/16.
  */
 
-public class SoundCloudApiInterface {
+public interface SoundCloudApiInterface {
+    @GET("user/{username}")
+    Observable<UserResponse> getUser(@Path("username") String username);
 
 }
--- app/src/main/java/com/brandonjf/cloudtrack/viewmodel/MainViewModelContract.java
+++ app/src/main/java/com/brandonjf/cloudtrack/viewmodel/MainViewModelContract.java
@@ -1,8 +1,19 @@
 package com.brandonjf.cloudtrack.viewmodel;
 
+import android.content.Context;
+
 /**
  * Created by brandon on 11/1/16.
  */
 
-public class MainViewModelContract {
+public interface MainViewModelContract {
+    interface View {
+        Context getContext();
+        void onLoadDataClicked();
+    }
+
+    interface ViewModel {
+        void destroy();
+    }
+
 }
