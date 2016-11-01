ee14b9c31eebe7fc755035838aeff1082dd62bfa
Brandon John-Freso <brandon@okcupid.com>
2016-10-31 21:39:33 -0400

    Adding MainviewModel

---
+++ app/src/main/java/com/brandonjf/cloudtrack/viewModels/MainViewModel.java
@@ -0,0 +1,10 @@
+package com.brandonjf.cloudtrack.viewModels;
+
+import android.databinding.BaseObservable;
+
+/**
+ * Created by brandon on 10/31/16.
+ */
+
+public class MainViewModel extends BaseObservable {
+}
