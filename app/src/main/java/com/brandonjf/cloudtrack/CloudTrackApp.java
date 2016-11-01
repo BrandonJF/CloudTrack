f37bfd8f38cb1464d4dda6363d1e30215eb8aa05
Brandon John-Freso <brandon@okcupid.com>
2016-10-31 20:18:30 -0400

    Reading properties through a file stream to protect them from VCS.

--- app/src/main/java/com/brandonjf/cloudtrack/CloudTrackApp.java
+++ app/src/main/java/com/brandonjf/cloudtrack/CloudTrackApp.java
@@ -1,6 +1,12 @@
 package com.brandonjf.cloudtrack;
 
 import android.app.Application;
+import android.util.Log;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
 
 import okhttp3.OkHttpClient;
 import okhttp3.logging.HttpLoggingInterceptor;
@@ -14,13 +20,43 @@ public class CloudTrackApp extends Application {
     public static final String TAG = CloudTrackApp.class.getName();
     public static CloudTrackApp mInstance;
     private Retrofit mRetrofit;
+    private Properties mProperties;
 
     @Override
     public void onCreate() {
         super.onCreate();
         mInstance = this;
+        try {
+            mProperties = loadProperties();
+            Log.d(TAG, "The properties loaded, the key is: " + getProperties().getProperty("testKey"));
+        } catch (IOException e) {
+            Log.e(TAG, "Exception: ", e);
+        }
+        setupRetrofit();
     }
 
+    public Properties getProperties() {
+        return mProperties;
+    }
+
+    private Properties loadProperties() throws IOException {
+        if (mProperties == null) {
+            mProperties = new Properties();
+        }
+        String[] fileList = {"stuff.properties"};
+        Properties properties = new Properties();
+        //We're not actually loading multiple prop files right now, but going to leave this here
+        //for ease you'd add an array.
+        String file = fileList[0];
+        try {
+            InputStream fileStream = getAssets().open(file);
+            properties.load(fileStream);
+            fileStream.close();
+        } catch (FileNotFoundException e) {
+            Log.e(TAG, "Can't find the stuff.properties file. Please create and try again.");
+        }
+         return properties;
+    }
     public void setupRetrofit() {
         OkHttpClient.Builder httpClient = new OkHttpClient.Builder();
         if (BuildConfig.DEBUG) {
